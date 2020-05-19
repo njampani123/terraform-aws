421c916ecbed92ba4512ab4d6ea59ee27021dffb
Naresh Jampani <jampani@adobe.com>
2020-05-19 16:35:50 -0700

    updating aws.tf

--- aws.tf
+++ aws.tf
@@ -4,29 +4,3 @@ secret_key = var.app_key
 profile = "default"
 region     = var.region
 }
-
-resource "aws_key_pair" "example" {
-  key_name   = "examplekey"
-  public_key = file(".ssh/terraform.pub")
-}
-
-resource "aws_instance" "example" {
-  key_name      = aws_key_pair.example.key_name
-  ami           = "ami-04590e7389a6e577c"
-  instance_type = "t2.micro"
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("~/.ssh/terraform")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras enable nginx1.12",
-      "sudo yum -y install nginx",
-      "sudo systemctl start nginx"
-    ]
-  }
-}
